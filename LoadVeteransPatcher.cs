659a05a9a8ac3374f0aeb44de0403130bbb8dd7b
Jamesthe1 <jamesthe1sky@gmail.com>
2024-12-07 15:36:49 -0600

    Fix variable name that caused a crash

--- LoadVeteransPatcher.cs
+++ LoadVeteransPatcher.cs
@@ -6,15 +6,15 @@ using HarmonyLib;
 namespace GameSaves {
     [HarmonyPatch (typeof (Faction), nameof (Faction.LoadVeteranUnitDataIntoPlayerFleet))]
     class LoadVeteransPatcher {
-        static bool Prefix (PlayerFleet fleet) {
-            if (fleet == null) return true;     // I do hope their programming practices have improved in this regard...
+        static bool Prefix (PlayerFleet pf) {
+            if (pf == null) return true;     // I do hope their programming practices have improved in this regard...
             try {
                 Savefile savefile = new Savefile (false);
                 Savefile.ResultUnion result = savefile.ReadFile ();
 
                 List<EmpireFlight> blankFlights = Globals.Instance.blankEmpireFlights;
-                for (int i = 0; i < blankFlights.Count && i < fleet.activeFlights.Count; i++)
-                    LoadSpecificUnitData (result, blankFlights[i].name, fleet.activeFlights[i]);
+                for (int i = 0; i < blankFlights.Count && i < pf.activeFlights.Count; i++)
+                    LoadSpecificUnitData (result, blankFlights[i].name, pf.activeFlights[i]);
             }
             catch (IOException ioe) {
                 GSPlugin.LogInstance.LogWarning ($"Could not load savefile in veterans step (using registry): {ioe.Message} (this can be safely ignored if a similar warning appeared before)");
@@ -39,7 +39,7 @@ namespace GameSaves {
             for (int i = 0; i < spawnPoints.Length && i < unitList.Count; i++) {
                 var entryTable = unitList[i].table;
                 SpawnPoint spawnPoint = spawnPoints[i];
-                
+
                 spawnPoint.callsign = entryTable["flightname"].strResult;
                 spawnPoint.experience = entryTable["flightxp"].intResult;
             }
